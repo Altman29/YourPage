1c4557234e792ede51194b0373d9e6fe93d55138
altman29 <s1yuan_chen@163.com>
2021-01-22 18:41:13 +0800

    init

---
+++ ReadMe.md
@@ -0,0 +1,19 @@
+My HomePage
+https://api.github.com/users/Altman29
+
+---
+
+Tech-Stack
+    Kotlin
+    Retrofit
+    JetPack
+        Navigation
+        LiveData
+        LifeCycle
+        ViewModel
+        Room
+    swipeRefreshLayout
+    circleImageView
+        
+Architecture
+    MVVM
---
+++ app/src/main/java/com/altman29/yourpage/MainActivity.java
@@ -0,0 +1,14 @@
+package com.altman29.yourpage;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
